2dc840e7e3ce01a86d3ca6924c64e8c6faca21ae
antisepticsprays <andrewc509@nycstudents.net>
2025-04-09 13:01:33 +0000

    pass all tests

--- src/main/java/com/example/project/RecursionChallenge.java
+++ src/main/java/com/example/project/RecursionChallenge.java
@@ -3,44 +3,91 @@
     public class RecursionChallenge {
 
             public static int bunnyEars(int n){
-                return 1;
+                if (n == 1) {
+                    return 2;
+                } else {
+                    return 2 + bunnyEars(n - 1);
+                }
             }
         
             public static int factorial(int n){
-                return 1;
+                if (n == 1) {
+                    return n;
+                } else {
+                    return n * factorial(n - 1);
+                }
             }
 
             public static int sumNumbers(int n){
-                return 1;
+                if (n == 1) {
+                    return n;
+                } else {
+                    return n + sumNumbers(n - 1);
+                }
             }
 
             public static String countDown(int n){
-                return "";
+
+                return "Blast Off!";
             }
 
             public static int power(int x, int n){
-                return 1;
+                if (n == 1) {
+                    return x;
+                } else {
+                    return x * power(x, n - 1);
+                }
             }
 
             public static int fib(int n){
-                return 1;
+                if (n <= 1) {
+                    return n;
+                }else{
+                    return fib(n - 1) + fib(n - 2);
+                }
             }
 
             public static int countX(String s){
-                return 1;
+                int count = 0;
+                if (s.length() == 0) {
+                    return count;
+                } else {
+                    if (s.substring(0,1).equals("x")) {
+                        count++;
+                    }
+                    return count + countX(s.substring(1));
+                }
             }
 
             public static String changePi(String s){
-                return "";
+                String newString = "";
+                if (s.length() == 0) {
+                    return newString;
+                } else {
+                    if (s.startsWith("pi")) {
+                        newString += "3.14";
+                        return newString + changePi(s.substring(2));
+                    }else{
+                        newString += s.substring(0,1);
+                        return newString + changePi(s.substring(1));
+                    }
+                }
             }
 
 
             public static String reverse(String s){
-                return "";
+                if (s.length() == 0) {
+                    return s;
+                } else {
+                    return reverse(s.substring(1)) + s.substring(0,1);
+                }
             }
 
             public static Boolean isPalindrome(String s){
-                return false;
+                if (reverse(s).equals(s)) {
+                    return true;
+                }else{
+                    return false;
+                }
             }
-
     }
